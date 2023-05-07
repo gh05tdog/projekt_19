5faa026c020fcfe947053e1fba958f84fb210fc3
Oliver Badike Hansen <s224754@dtu.dk>
2023-05-07 16:59:13 +0200

    Changed it so its no longer button ion projectPageController

--- src/main/java/application/projectPageController.java
+++ src/main/java/application/projectPageController.java
@@ -4,14 +4,15 @@ package application;
 import app.CSVgenerator;
 import app.SoftwareApp;
 import domain.Project;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -23,23 +24,17 @@ public class projectPageController {
     public Label projectNameLabel;
 
     public Label projectIDLabel;
+    public CSVgenerator csVgenerator;
     private Model theModel;
-
     @FXML
     private ScrollPane projectScrollPane;
-
     @FXML
     private VBox vBoxName;
-
     @FXML
     private VBox vBoxID;
-
     @FXML
     private VBox vBoxTime;
-
     private View view;
-
-    public CSVgenerator csVgenerator;
     @FXML
     private TextField getDate;
 
@@ -62,40 +57,32 @@ public class projectPageController {
         Project currentProject = SoftwareApp.getProject(projectIDLabel.getText());
         assert currentProject != null;
 
-
         for (int i = 0; i < currentProject.getActivityList().size(); i++) {
             Project.Activities activity = currentProject.getActivityList().get(i);
             final String activityId = activity.getActivityId();
             final String activityName = activity.getActivityName();
 
-            Button activityButtonName = new Button();
-            activityButtonName.setMinWidth(vBoxName.getWidth());
-            activityButtonName.maxWidthProperty().bind(vBoxName.widthProperty());
-            activityButtonName.setText(activityName);
-            activityButtonName.setId(activityId); // set the ID of the button to the activity ID
+            Button activityButton = new Button(activityName);
+            activityButton.setMinWidth(vBoxName.getWidth());
+            activityButton.maxWidthProperty().bind(vBoxName.widthProperty());
+            activityButton.setOnAction(e -> view.showActivityPage(activity.getActivityId(), activity.getActivityName(), projectIDLabel.getText()));
 
-            activityButtonName.setOnAction(e -> {
-                
-                view.showActivityPage(activity.getActivityId(), activity.getActivityName(), projectIDLabel.getText()); // use activity's properties directly
-            });
+            Label idLabel = new Label(activityId);
+            idLabel.setMinWidth(vBoxID.getWidth());
+            idLabel.maxWidthProperty().bind(vBoxID.widthProperty());
+            idLabel.setPadding(new Insets(4, 4, 4, 5)); // add left padding of 10 pixels and bottom margin of 5 pixels
 
+            Label timeLabel = new Label(activity.getTimeBudget());
+            timeLabel.setMinWidth(vBoxTime.getWidth());
+            timeLabel.maxWidthProperty().bind(vBoxTime.widthProperty());
+            timeLabel.setPadding(new Insets(4, 4, 4, 5)); // add left padding of 10 pixels and bottom margin of 5 pixels
 
-            Button activityButtonId = new Button();
-            activityButtonId.setMinWidth(vBoxName.getWidth());
-            activityButtonId.maxWidthProperty().bind(vBoxName.widthProperty());
-            activityButtonId.setText(activityId);
-
-            Button activityButtonTime = new Button();
-            activityButtonTime.setMinWidth(vBoxName.getWidth());
-            activityButtonTime.maxWidthProperty().bind(vBoxName.widthProperty());
-            activityButtonTime.setText(activity.getTimeBudget());
-
-            // set button properties
-            vBoxName.getChildren().add(activityButtonName);
-            vBoxID.getChildren().add(activityButtonId);
-            vBoxTime.getChildren().add(activityButtonTime);
+            vBoxName.getChildren().add(activityButton);
+            vBoxID.getChildren().add(idLabel);
+            vBoxTime.getChildren().add(timeLabel);
         }
     }
+
 
 
 
@@ -114,22 +101,17 @@ public class projectPageController {
 
     @FXML
     protected void manageProjectPagePressed() {
-        theModel.manageProject(projectIDLabel.getText(),projectNameLabel.getText());
+        theModel.manageProject(projectIDLabel.getText(), projectNameLabel.getText());
 
     }
-
     public void generateReportPressed() throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-      Date date = dateFormat.parse(getDate.getText());
-      Calendar calendar = Calendar.getInstance();
-      calendar.setTime(date);
-      int week = calendar.get(Calendar.WEEK_OF_YEAR);
-      csVgenerator = new CSVgenerator(SoftwareApp.getProject(projectIDLabel.getText()));
-      csVgenerator.saveCSVReportToFile(week);
-      
-
+        Date date = dateFormat.parse(getDate.getText());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int week = calendar.get(Calendar.WEEK_OF_YEAR);
+        csVgenerator = new CSVgenerator(SoftwareApp.getProject(projectIDLabel.getText()));
+        csVgenerator.saveCSVReportToFile(week);
     }
-
-
 
 }
